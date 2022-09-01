f711e493a69246cfb59a6446adf0e77a64a6cec1
Dario Cruz <rio2012@gmail.com>
2022-09-01 15:51:44 -0400

    Add code for webcam functionality.

--- silverfox-webhook.py
+++ silverfox-webhook.py
@@ -2,9 +2,11 @@ import json
 from multiprocessing import pool
 import re
 from tkinter.tix import Tree
+from cv2 import VideoCapture, imshow
 import requests
 import pyautogui
 import time
+import cv2
 
 # Webhook url for slack intergration.
 webhook_url = 'https://hooks.slack.com/workflows/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'
@@ -19,12 +21,19 @@ data = { 'Detection': 'MC33 dectected passing the security desk!' }
 # Minimize window after script execute.
 pyautogui.hotkey("win", 'down')
 
+# Setting up camera functionality.
+# Will display a still image of the last person walking out with scanner device. 
+cam_port = 1
+cam = VideoCapture(cam_port)
+result, image = cam.read()
+
 # Code for hook execution. Will detect if alarm is sounded. 
 while 1:
     if pyautogui.locateOnScreen('target.png') != None:
         print("I can see it!")
-        requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        # requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        imshow("Person Cap", image)
         time.sleep(5)
     else:
         print("Nope,I can't see it!")
-        time.sleep(5)
+        time.sleep(1)
